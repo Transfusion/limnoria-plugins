bf90f4e806dc9de78ee6fecc59a5637ba6340bf9
Transfusion <transfusian@gmail.com>
2013-05-29 16:54:10 +0800

    Update plugin.py

--- YTInfo/plugin.py
+++ YTInfo/plugin.py
@@ -33,7 +33,7 @@ def video_id(value):
         return query.path[1:]
     if query.hostname in ('www.youtube.com', 'youtube.com'):
         if query.path == '/watch':
-            p = parse_qs(query.query)
+            p = urlparse.parse_qs(query.query)
             return p['v'][0]
         if query.path[:7] == '/embed/':
             return query.path.split('/')[2]
